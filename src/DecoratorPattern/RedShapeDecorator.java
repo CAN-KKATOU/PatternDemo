7f99872c50e613c3ec5fb49ebaaf68ed95965f8f
Astria <1752651025@qq.com>
2022-11-24 22:51:08 +0800

    Add Facade and Flyweight Pattern

---
+++ src/DecoratorPattern/RedShapeDecorator.java
@@ -0,0 +1,17 @@
+package DecoratorPattern;
+
+public class RedShapeDecorator extends ShapeDecorator {
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setRedColor(decoratedShape);
+    }
+
+    private void setRedColor(Shape decoratedShape) {
+        System.out.println("Border Color: Red");
+    }
+}
---
+++ src/FacadePattern/FacadePatternDemo.java
@@ -0,0 +1,11 @@
+package FacadePattern;
+
+public class FacadePatternDemo {
+    public static void main(String[] args) {
+        var shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
